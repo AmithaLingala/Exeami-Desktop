70f538a80e1eff8c4e6d86a3741b327c258e0979
rahul-s <rahul.s@hp.com>
2020-02-13 20:41:12 +0530

    Modified terminal UI

--- script/main.js
+++ script/main.js
@@ -29,25 +29,32 @@ const commands = [
     }
 
 ]
-
+const cursor = document.createElement('div');
+cursor.setAttribute('class', 'cursor');
 async function executeCode() {
     for ( let i = 0; i < commands.length; i++) {
         const item = commands[i];
-        const code = document.createElement('span', { class: 'code' });
-        code.innerHTML = '$ ' + item.command + "<br>";
+        const code = document.createElement('span');
+        code.setAttribute('class', 'code')
+        code.innerHTML = '$ ' + item.command;
         termBox.append(code);
+        termBox.append(cursor);
+        termBox.append(document.createElement('br'));
         await sleep(1000);
 
         for (result of item.results) {
-            const res = document.createElement('span', { class: 'code' });
+            const res = document.createElement('span');
+            res.setAttribute('class', 'code')
             res.innerHTML = result + "<br>";
             termBox.append(res);
+            termBox.append(cursor);
             await sleep(1000);
         }
         const pathClone = document.getElementById('userpath').cloneNode(true);
         pathClone.innerHTML =  "<br>" + pathClone.innerHTML;
         if(i  < commands.length-1){
             termBox.append(pathClone);
+            termBox.append(cursor);
         }
         
         await sleep(2000);
