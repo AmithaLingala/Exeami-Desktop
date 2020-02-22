7c1cbae506edf695f03f4eca090ae199933d00a7
CodingOtaku <contact@codingotaku.com>
2020-02-22 16:09:36 +0530

    added close action, logo now comments when no windows are open

--- script/main.js
+++ script/main.js
@@ -1,7 +1,12 @@
 const title = document.getElementById('title');
+const close = document.getElementById('close');
 const desktop = document.getElementById('desktop');
+const windowBox = document.getElementById('window-box');
+const logo = document.getElementById('logo');
+const bubble = document.getElementById('bubble');
 
 let windowPane = document.getElementById('window-pane');
+
 let folders = [];
 
 fetch("/data/folders.json")
@@ -10,10 +15,16 @@ fetch("/data/folders.json")
         folders = json.folders;
         generateFolderLinks();
     });
+close.onclick = () => {
+  windowBox.classList.add('hide')
+  logo.classList.remove('logo');
+  logo.classList.add('lion');
+  bubble.classList.remove('hide');
+};
 
 function generateFolderLinks() {
     for (let folder of folders) {
-        // Create a Folder div for every path in folders.json 
+        // Create a Folder div for every path in folders.json
         const iconLabel = document.createElement('span');
         const folderDiv = document.createElement('div');
         const icon = document.createElement('span');
@@ -34,6 +45,10 @@ function generateFolderLinks() {
                 /** The following is a workaround since neither chrome nor edge supports latest embed standard
                  * We were unable to change embed src dynamically, so here we clone the embed element and replace its src before replacing the embed itself.
                  */
+                windowBox.classList.remove('hide');
+                logo.classList.add('logo');
+                  logo.classList.remove('lion');
+                bubble.classList.add('hide');
                 const clone = windowPane.cloneNode(true);
                 clone.setAttribute('src', folder.name);
                 windowPane.parentNode.replaceChild(clone, windowPane)
